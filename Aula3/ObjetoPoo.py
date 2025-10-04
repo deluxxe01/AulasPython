4d8e494319ae1305a6b3fffe8f7e3ee6b400f414
deluxxe01 <dudaklincarvalho@gmail.com>
2025-10-04 14:07:27 -0300

    Orientação a objeto

--- Aula3/ObjetoPoo.py
+++ Aula3/ObjetoPoo.py
@@ -1,3 +1,4 @@
+#Criando uma classe
 class Pessoa: 
     def __init__(self, nome, idade, altura):
         self.nome = nome
@@ -12,6 +13,25 @@ objeto_pessoa2 = Pessoa("Duda", 18, 1.65)
 objeto_pessoa3 = Pessoa("Pedro", 5, 1.20)
 
 print(objeto_pessoa1.nome)
-objeto_pessoa1.imprimirDados()
-objeto_pessoa2.imprimirDados()
-objeto_pessoa3.imprimirDados()
+objeto_pessoa1.imprimirDados() #Resultado: Ruan - 25 - 1.6
+objeto_pessoa2.imprimirDados() #Resultado: Duda - 18 - 1.65
+objeto_pessoa3.imprimirDados() #Resultado: Pedro - 5 - 1.2
+
+#Criando uma classe
+class vendedor():
+    def __init__(self, nome):
+        self.nome = nome
+        self.vendas = 0
+
+    def vendeu(self, vendas):
+        self.vendas = vendas
+
+    def bateu_meta(self, meta):
+        if self.vendas > meta:
+            print(self.nome,"Bateu a meta")
+        else: 
+            print(self.nome,"Não bateu a meta")
+
+vendedor1 = vendedor("Lira")
+vendedor1.vendeu(1000)
+vendedor1.bateu_meta(800)
